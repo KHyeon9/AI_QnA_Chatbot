4df7405a18cae5fb5ea8638b61ce358360491bab
git에서 Khyeon9 <git mrk19967@gmail.com>
2025-10-29 20:37:30 +0900

    feet : QnA Chatbot 초기 셋팅

---
+++ QnA_Chatbot_Back/src/main/java/com/chatbot/qna/QnAChatbotBackApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.qna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnAChatbotBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnAChatbotBackApplication.class, args);
+	}
+
+}
---
+++ QnA_Chatbot_Back/src/test/java/com/chatbot/qna/QnAChatbotBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.qna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnAChatbotBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
